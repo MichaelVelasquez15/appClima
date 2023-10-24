045637786d7f0c577f510b0ce6c44d76cb30df94
MichaelVelasquez15 <80552624+MichaelVelasquez15@users.noreply.github.com>
2023-10-23 20:03:42 -0500

    apis

--- Cclima/src/components/Card.jsx
+++ Cclima/src/components/Card.jsx
@@ -1,23 +1,76 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 function Card() {
+  const KEY = "4034a0257120e649cb4d522b971c740c";
+  const [lat, setLat] = useState(null);
+  const [long, setLong] = useState(null);
+  const [current, setCurrent] = useState(null);
+
+  useEffect(() => {
+    if (lat === null && long === null) return;
+
+    console.log("fetch de datos");
+    const getData = async () => {
+      const link = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${KEY}&units=metric`;
+      const res = await fetch(link);
+      const data = await res.json();
+
+      console.log(data);
+      setCurrent(data);
+    };
+    getData();
+  }, [lat, long]);
+
+  const handleSucces = (data) => {
+    console.log("tenemos la ubicacion", data);
+    const { latitude, longitude } = data.coords;
+    setLat(latitude);
+    setLong(longitude);
+  };
+
+  const handleError = () => {
+    console.log("Ubicacion denegada");
+  };
+
+  const handleLocation = () => {
+    navigator.geolocation.getCurrentPosition(handleSucces, handleError);
+  };
+
   return (
     <section className="layout">
       <div className="sidebar">
-        <nav>
-          <button>1</button>
-          <button>2</button>
+        <nav className="nav1">
+          <button className="bt1">Search for places</button>
+          <button className="bt2" onClick={handleLocation}>
+            <span className="material-symbols-outlined">my_location</span>
+          </button>
         </nav>
-        <div className="fondo"></div>
-        <div className="logo">
-          <img src="weather-app-master/Clear.png" alt="" />
+
+        <div className="caja">
+          <div className="fondo">
+            <img src="/public/weather-app-master/Cloud-background.png" alt="" />
+          </div>
+          <div className="logo">
+            <img src="weather-app-master/Clear.png" alt="" />
+          </div>
         </div>
+
         <div>
-          <p>estado</p>
+          <p>
+            {current === null ? "da click al boton plis" : current.main.temp}
+          </p>
         </div>
+
         <div>
           <p>fecha hoy</p>
         </div>
+
         <div>
-          <p>ubicacion</p>
+          <p>
+            <span class="material-symbols-outlined">location_on</span>
+            {current === null ? "da click al boton plis" : current.name}
+          </p>
         </div>
       </div>
 
